e3bb23c5c233d318a1d762dc2c7790a3c906976a
katsuyama08 <katsuyama@ertl.jp>
2020-09-25 11:03:34 +0900

    20200925

---
+++ common/tSampleTaskMain.c
@@ -0,0 +1,54 @@
+/*
+ * このファイルは tecsgen によりテンプレートとして自動生成されました
+ * このファイルを編集して使用することが意図されていますが
+ * tecsgen の再実行により上書きされてしまうため、通常
+ *   gen/tSampleTaskMain_templ.c => src/tSampleTaskMain.c
+ * のように名前, フォルダを変更してから修正します
+ */
+/* #[<PREAMBLE>]#
+ * #[<...>]# から #[</...>]# で囲まれたコメントは編集しないでください
+ * tecsmerge によるマージに使用されます
+ *
+ * #[</PREAMBLE>]# */
+
+/* プロトタイプ宣言や変数の定義をここに書きます #_PAC_# */
+#include "tSampleTaskMain_tecsgen.h"
+
+#ifndef E_OK
+#define	E_OK	0		/* success */
+#define	E_ID	(-18)	/* illegal ID */
+#endif
+
+/* 受け口関数 #_TEPF_# */
+/* #[<ENTRY_PORT>]# eTaskBody
+ * entry port: eTaskBody
+ * signature:  nLUM_sTaskBody
+ * context:    task
+ * #[</ENTRY_PORT>]# */
+
+/* #[<ENTRY_FUNC>]# eTaskBody_main
+ * name:         eTaskBody_main
+ * global_name:  tSampleTaskMain_eTaskBody_main
+ * oneway:       false
+ * #[</ENTRY_FUNC>]# */
+void
+eTaskBody_main(CELLIDX idx)
+{
+	CELLCB	*p_cellcb;
+	if (VALID_IDX(idx)) {
+		p_cellcb = GET_CELLCB(idx);
+	}
+	else {
+		/* エラー処理コードをここに記述します */
+	} /* end if VALID_IDX(idx) */
+
+	/* ここに処理本体を記述します #_TEFB_# */
+	lcdfont_t font = EV3_FONT_MEDIUM;
+	ev3_lcd_set_font(font);
+	ev3_lcd_draw_string("LCDSample", 0, 20);
+	
+}
+
+/* #[<POSTAMBLE>]#
+ *   これより下に非受け口関数を書きます
+ * #[</POSTAMBLE>]#*/
